2dead59a5b483391b0e69721a80f718fa93b20c4
Manre-PC\Manre <jmmg37@gcloud.ua.es>
2017-09-30 01:16:49 +0200

    AHORA SÍ se muestra el tablero

--- Código/MagantoGames/Assets/Scripts/Tablero.cs
+++ Código/MagantoGames/Assets/Scripts/Tablero.cs
@@ -2,26 +2,53 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Tablero : MonoBehaviour {
+public class Tablero : MonoBehaviour
+{
 
-    private int[][] matrix;
+    private int[][] matrix = new int[17][];
     public int map;
-	// Use this for initialization
-	void Start () {
-        for(int i = 0; i< 17; i++)
+    public GameObject[] floorTiles;
+    public float tamX;
+    public float tamY;
+
+    // Use this for initialization
+    void Start()
+    {
+        for (int i = 0; i < 17; i++)
         {
-            for(int j= 0; j< 18; j++)
+            matrix[i] = new int[18];
+            for (int j = 0; j < 18; j++)
             {
                 matrix[i][j] = 0;
             }
         }
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if(map == 1)
+        creaMapa();
+    }
+
+
+    void creaMapa()
+    {
+        int matrixFila = matrix.Length, matrixColumna = matrix[0].Length;
+
+        for (int i = 0; i < matrixFila; i++)
+        {
+            for (int j = 0; j < matrixColumna; j++)
+            {
+                GameObject losetilla = floorTiles[Random.Range(0, floorTiles.Length)];
+
+                Instantiate(losetilla, new Vector2(i * tamX, j * tamY), Quaternion.identity);
+            }
+        }
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (map == 1)
         {
 
         }
-	}
+    }
 }
