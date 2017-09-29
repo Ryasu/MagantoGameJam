e6dcca602356024d11e962f5578999160c814d1e
Manre-PC\Manre <jmmg37@gcloud.ua.es>
2017-09-30 01:05:34 +0200

    Se muestran los sprites del suelo

--- Código/MagantoGames/Assets/Scripts/Tablero.cs
+++ Código/MagantoGames/Assets/Scripts/Tablero.cs
@@ -4,19 +4,43 @@ using UnityEngine;
 
 public class Tablero : MonoBehaviour {
 
-    private int[][] matrix;
+    private int[][] matrix = new int[17][];
     public int map;
-	// Use this for initialization
-	void Start () {
-        for(int i = 0; i< 17; i++)
+    public GameObject[] floorTiles;
+    public float tamX;
+    public float tamY;
+
+    // Use this for initialization
+    void Start () {
+        for(int i = 0; i < 17; i++)
         {
-            for(int j= 0; j< 18; j++)
+            matrix[i] = new int[18];
+            for(int j= 0; j < 18; j++)
             {
                 matrix[i][j] = 0;
             }
         }
+        creaMapa();
 	}
 	
+
+    void creaMapa()
+    {
+        int matrixFila = matrix.Length, matrixColumna = matrix[0].Length;
+
+        for(int i = 0; i < matrixFila; i++)
+        {
+            for(int j = 0; j < matrixColumna; j++)
+            {
+                GameObject losetilla = floorTiles[Random.Range(0, floorTiles.Length)];
+
+                Instantiate(losetilla, new Vector2(i*tamX, j*tamY), Quaternion.identity);
+            }
+        }
+    }
+
+
+
 	// Update is called once per frame
 	void Update () {
 		if(map == 1)
